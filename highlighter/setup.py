79e6dc4b7ec3b17cac56d22a8bdd18c1e5648091
Matvey <matvey.ivchenko@yandex.ru>
2025-05-03 14:27:47 +0300

    код для трейна эмбеддингов

---
+++ highlighter/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="highlighter",
+    version="0.0.0",
+    packages=find_packages(),
+    description="Библиотека для выделения моментов",
+    author="-",
+    author_email="-",
+    install_requires=[],  # Зависимости (например: ["requests>=2.25.1"])
+    python_requires=">=3.8",  # Минимальная версия Python
+)
